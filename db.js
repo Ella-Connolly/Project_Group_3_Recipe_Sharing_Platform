905a09fcb9fb8551d867fe5152e0dc6734e242fe
Amilya Yirsa <agy26@nau.edu>
2025-12-07 17:17:22 -0700

    Fix database issues

--- db.js
+++ db.js
@@ -1,16 +1,18 @@
-const { Pool } = require("pg");
-require("dotenv").config();
 
+// db.js
+require("dotenv").config();
+const { Pool } = require("pg");
+
+// Use environment variables if present, otherwise fallback to defaults
 const pool = new Pool({
-  user: "josselin",
-  password: "newpassword",
-  host: "localhost",
-  port: 5432,
-  database: "recipeshare"
+  user: process.env.PGUSER || "postgres",
+  password: process.env.PGPASSWORD || "Nachos23!",
+  host: process.env.PGHOST || "localhost",
+  port: process.env.PGPORT || 5432,
+  database: process.env.PGDATABASE || "recipe",
 });
 
 module.exports = {
   query: (text, params) => pool.query(text, params),
   pool,
-};
-
+}
